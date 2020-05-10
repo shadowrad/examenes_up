bea8b253a70f18023d787fd517951a86e4711029
shadowrad <duranrodrigo@gmail.com>
2020-05-10 18:47:30 -0300

    cambios

--- generador_pdf/views.py
+++ generador_pdf/views.py
@@ -6,9 +6,15 @@ from preguntas.models import Pregunta, Materia
 
 
 def parcial_ean(request):
-    materias = Materia.objects.all()
-    materia_id = 3
-    randoms_id = examen_preguntas_ids(5, 3)
-    preguntas = Pregunta.objects.filter(id__in=randoms_id)
+    materia = None
+    materias = []
+    preguntas = []
+    if request.method == "POST":
+        materia_id = request.POST.get('materia_id')
+        materia = Materia.objects.get(pk=materia_id)
+        randoms_id = examen_preguntas_ids(5, materia_id)
+        preguntas = Pregunta.objects.filter(id__in=randoms_id)
+    else:
+        materias = Materia.objects.all()
 
-    return render(request,'parcial_otro.html', {'preguntas':preguntas,'materias':materias})
+    return render(request,'parcial_otro.html', {'preguntas':preguntas,'materias':materias, 'materia': materia})
